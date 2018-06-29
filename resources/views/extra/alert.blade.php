94274388846b85f9e3e916e0648dcc4eff9c2413
David Douglas <ddouglas@douglaswebdev.net>
2018-06-29 11:48:43 -0400

    Made a small adjustment to the size of the header in alerts

--- resources/views/extra/alert.blade.php
+++ resources/views/extra/alert.blade.php
@@ -4,7 +4,7 @@
             <div class="alert alert-dismissable alert-{{ Session::get('alert.type') }}">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 @if(Session::has('alert.header'))
-                    <h3>{{ Session::get('alert.header') }}</h3>
+                    <h4>{{ Session::get('alert.header') }}</h4>
                 @endif
                 @if(Session::has('alert.message'))
                     <p>{!! Session::get('alert.message') !!}</p>
