91badd080f8a27a041469e8cefbb366e9fa11110
David Douglas <ddouglas@douglaswebdev.net>
2018-06-29 11:52:24 -0400

    Routes for core functionaility

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,22 @@ Route::match(['GET'],'/logout', 'AuthController@logout')->name('auth.logout');
 Route::match(['GET'],'/sso/callback', 'SSOController@callback')->name('sso.callback');
 Route::match(['GET'],'/sso/revoke', 'SSOController@revoke')->name('sso.revoke');
 Route::match(['GET'],'/sso/refresh', 'SSOController@refresh')->name('sso.refresh');
+
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::match(['GET'], '/dashboard', 'PortalController@dashboard')->name('dashboard');
+    Route::match(['GET', 'POST'], '/welcome', 'PortalController@welcome')->name('welcome');
+
+    Route::match(['GET'], '/{member}/overview', 'PortalController@overview')->name('overview')->middleware("authorized");
+    Route::match(['GET'], '/{member}/clones', 'PortalController@clones')->name('clones')->middleware("authorized:esi-clones.read_clones.v1");
+    Route::match(['GET'], '/{member}/skills', 'PortalController@skills')->name('skillz')->middleware("authorized:esi-skills.read_skills.v1");
+    Route::match(['GET'], '/{member}/skills/flyable', 'PortalController@flyable')->name('skillz.flyable')->middleware("authorized:esi-skills.read_skills.v1");
+    Route::match(['GET'], '/{member}/skillqueue', 'PortalController@queue')->name('skillqueue')->middleware("authorized:esi-skills.read_skillqueue.v1");
+
+    Route::match(['GET'], '/settings', 'SettingController@index')->name('settings.index');
+    Route::match(['GET', 'DELETE'], '/settings/token', 'SettingController@token')->name('settings.token');
+    
+});
+
+
+Route::match(['GET'], '/hack', 'HackingController@index');
