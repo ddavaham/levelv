00de7e88e127c166b5760cff7e9348ab9ddd4e57
David Douglas <ddouglas@douglaswebdev.net>
2018-08-05 06:13:05 -0400

    Changes to the overall structure of the model

--- app/Models/SkillPlan.php
+++ app/Models/SkillPlan.php
@@ -4,7 +4,7 @@ namespace LevelV\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-use LevelV\Models\ESI\Type;
+use LevelV\Models\ESI\Character;
 
 class SkillPlan extends Model
 {
@@ -13,34 +13,40 @@ class SkillPlan extends Model
     public $incrementing = false;
     protected static $unguarded = true;
 
-    public function skillz()
-    {
-        return $this->hasMany(SkillPlanSkillz::class, 'plan_id')->with('info')->orderBy('skillplan_skillz.position', 'asc');
-    }
+    protected $with = ['author'];
 
     public function getAttributesAttribute($attributes)
     {
         return collect(json_decode($attributes, true))->recursive();
     }
 
-    public function getTotalSpAttribute($total_sp)
-    {
-        return number_format($total_sp, 0) . " SP";
-    }
-
-    public function getTrainingTimeAttribute($training_time)
-    {
-        $day = floor ($training_time / 1440);
-        $hour = floor (($training_time - $day * 1440) / 60);
-        $min = $training_time - ($day * 1440) - ($hour * 60);
-        return number_format($day, 0). "d ".number_format($hour, 0)."h ".number_format($min, 0)."m";
-    }
     public function getRemapsAttribute($remaps)
     {
         return collect(json_decode($remaps, true));
+    }
+
+    public function author()
+    {
+        return $this->hasOne(Character::class, 'id', 'author_id');
+    }
+
+    public function skillz()
+    {
+        return $this->hasMany(SkillPlanSkillz::class, 'plan_id')->with('info')->orderBy('skillplan_skillz.position', 'asc');
+    }
+
     public function members()
     {
         return $this->hasMany(SkillPlanMembers::class, 'plan_id')->with('info');
     }
+
+    public function isPrivate()
+    {
+        return !$this->is_public;
+    }
+
+    public function isPublic()
+    {
+        return $this->is_public;
     }
 }
