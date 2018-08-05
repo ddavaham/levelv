001ee8b90c6939552d1c42645e3e8eac7b554b97
David Douglas <ddouglas@douglaswebdev.net>
2018-08-05 05:52:11 -0400

    Removing a commented out block of code from the login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,32 +19,6 @@
                         </a>
                     </div>
                 </div>
-                {{-- <h4 class="mt-2">Patch Notes</h4>
-                <hr class="mt-0" />
-                <div id="accordion">
-                    <div class="card">
-                        <div class="card-header" id="headingOne" data-toggle="collapse" data-target="#v020_beta">
-                            <span>
-                                2018-06-23 - v0.21-beta
-                            </span>
-                        </div>
-                        <div id="v020_beta" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-                            <div class="card-body">
-                                <p>
-                                    The following addresses the bugs and feature requests that were released in the <a href="{{ config('services.bitbucket.urls.commit') }}/tag/v0.21-beta">release</a>. If you have any question, please reference the #TalkWithTheDeveloper section of the about page.
-                                </p>
-                                <ul>
-                                    <li>
-                                        Updated this page with these patch notes
-                                    </li>
-                                    <li>
-                                        Added Link to URL management page on the Setting Navigation Menu
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
             </div>
         </div>
     </div>
