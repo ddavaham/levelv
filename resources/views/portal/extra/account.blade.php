0352797f78b7fb449db11e8ec4d433fee220a407
David Douglas <ddouglas@douglaswebdev.net>
2018-08-05 05:59:54 -0400

    Removed teh skillplan manager menu option from the character nav to the account nav

--- resources/views/portal/extra/account.blade.php
+++ resources/views/portal/extra/account.blade.php
@@ -1,6 +1,6 @@
 <h3>Sub Navigation</h3>
 <hr />
 <div class="list-group">
-    <a href="{{ route('skillplans.list') }}" class="list-group-item list-group-item-action" target="_blank">Skill Plan Manager</a>
+    <a href="{{ route('skillplans.list') }}" class="list-group-item list-group-item-action">Skill Plan Manager</a>
     {{-- <a href="{{ route('fittings.list') }}" class="list-group-item list-group-item-action" target="_blank">Fitting Manager</a> --}}
 </div>
