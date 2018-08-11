7e8b2d8bddb2e8d0507faf14d9fec2d4d681f140
David Douglas <ddouglas@douglaswebdev.net>
2018-08-11 11:49:34 -0400

    This file is very important

---
+++ resources/views/extra/skillplans/example.blade.php
@@ -0,0 +1,44 @@
+Spaceship Command I <br />
+Minmatar Frigate I <br />
+Minmatar Frigate II <br />
+Minmatar Frigate III <br />
+Minmatar Destroyer I <br />
+Minmatar Destroyer II <br />
+Minmatar Destroyer III <br />
+Spaceship Command II <br />
+Minmatar Cruiser I <br />
+Minmatar Cruiser II <br />
+Minmatar Cruiser III <br />
+Minmatar Cruiser IV <br />
+Minmatar Cruiser V <br />
+Minmatar Strategic Cruiser I <br />
+Power Grid Management I <br />
+Shield Operation I <br />
+Shield Operation II <br />
+Shield Operation III <br />
+Mechanics I <br />
+Mechanics II <br />
+Mechanics III <br />
+Mechanics IV <br />
+Mechanics V <br />
+Minmatar Defensive Systems I <br />
+Navigation I <br />
+Navigation II <br />
+Navigation III <br />
+Navigation IV <br />
+Navigation V <br />
+Minmatar Propulsion Systems I <br />
+Power Grid Management II <br />
+Power Grid Management III <br />
+Power Grid Management IV <br />
+Power Grid Management V <br />
+Minmatar Core Systems I <br />
+Missile Launcher Operation I <br />
+Missile Launcher Operation II <br />
+Missile Launcher Operation III <br />
+Gunnery I <br />
+Gunnery II <br />
+Gunnery III <br />
+Gunnery IV <br />
+Gunnery V <br />
+Minmatar Offensive Systems I
